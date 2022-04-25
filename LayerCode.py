8082b56580b47a9a3f36105fb214b216a26430c5
clearlyCoding <75322943+clearlyCoding@users.noreply.github.com>
2022-04-24 20:11:44 -0400

    Neural Network

---
+++ LayerCode.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+#Data imports
+import nnfs
+from nnfs.datasets import spiral_data
+nnfs.init()
+
+
+
+class Layer_Dense:
+ 	def __init__(self, n_inputs, n_neurons):
+ 		self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+ 		self.biases = np.zeros((1, n_neurons))
+ 	def forward(self, inputs):
+ 		self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_ReLU: #rectified linear
+	def forward(self, inputs):
+		self.output = np.maximum(0,inputs)
+
+class Activation_softmax: #Softmax activation function
+	def forward(self, inputs):
+		exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims = True))
+		probabilities = exp_values /np.sum(exp_values, axis=1, keepdims = True)
+		self.output = probabilities
+
+class Loss:
+	def calculate(self, output, y):
+		sample_losses = self.forward(output, y)
+		data_loss = np.mean(sample_losses)
+		return data_loss
+
+class Loss_CategoricalCrossentropy (Loss):
+	def forward (Self, y_pred, y_true): #where y_true is class classification
+		samples = len(y_pred) #length of the output of the functions (output in lol)
+		y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7) #reduction from infinity but 
+		if len(y_true.shape)==1:
+			correct_confidences = y_pred_clipped[range(samples), y_true]
+		elif len(y_true.shape) ==2:
+			correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
+
+		negative_log_likelihoods = -np.log(correct_confidences)
+		return negative_log_likelihoods
+
+
+X, y = spiral_data(samples = 100,classes = 3)
+dense1 = Layer_Dense(2,3)
+activation1 = Activation_ReLU()
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+
+
+
+loss_function = Loss_CategoricalCrossentropy()
+loss = loss_function.calculate(activation2.output, y)
+
+print("Loss:", loss)
